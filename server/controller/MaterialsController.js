b76ab513908425f1ee04848fdcd7a15551c2cbad
Nisarg_Fedora <amlaninisarg15@gmail.com>
2025-02-23 01:30:12 +0530

    materials endpoint added and tested

--- server/controller/MaterialsController.js
+++ server/controller/MaterialsController.js
@@ -1,11 +1,23 @@
 const Material = require("../modals/Materials");
 
+const  generateRandomCode = (length) => {
+    const SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
+    let salt = "";
+    for (let i = 0; i < length; i++) {
+        const index = Math.floor(Math.random() * SALTCHARS.length);
+        salt += SALTCHARS.charAt(index);
+    }
+    return salt;
+}
+
+
 exports.uploadMaterial = async (req, res) => {
     try {
-        const { code, title, description, urls, file_names, class_id } = req.body;
-        const material = new Material({ class_id , title, description, urls, file_names, class_code: code });
+        const code = generateRandomCode(16);
+        const {  title, description, urls, file_names, class_id } = req.body;
+        const material = new Material({ class_id , title, description, urls, file_names,  code });
         await material.save();
-        res.json(await Material.find({ class_code: code }));
+        res.json(await Material.find({ class_id: class_id }));
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -23,8 +35,8 @@ exports.getMaterials = async (req, res) => {
 exports.deleteMaterial = async (req, res) => {
     try {
         const { class_code, material_code } = req.params;
-        await Material.findOneAndDelete({ uniqueCode: material_code });
-        res.json(await Material.find({ class_code }));
+        await Material.findOneAndDelete({ code: material_code });
+        res.json(await Material.find({ class_id:class_code }));
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
